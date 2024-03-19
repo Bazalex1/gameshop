482775c624e8c5f2f646772a2a220de41ff67ae8
Alex B <bazalex1@yandex.ru>
2024-03-19 19:15:33 +0300

    Добавил функцию удаления комментариев

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Comment
+from .forms import CommentForm
 
-# Register your models here.
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    form = CommentForm
+
+    list_display = ("user", "text", "game", "created_at")
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    exclude = ["created_at"]
+    extra = 1
+
+
+admin.site.register(Comment, CommentAdmin)
